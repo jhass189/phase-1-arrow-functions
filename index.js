af0e1b1b970e3b9b118ec03a9efc973fb2f0862c
James Hassett <jhassett189@gmail.com>
2022-06-06 21:58:56 -0400

    commit tree cd51d2276366b71a85f1f025830689f28cbbc1ff

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = function () {
+    let divide = 2000
+    return divide / 100
+}
+
+const square = function () {
+    let square = '2'
+    return square * square
+}
+
+
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add(2, 3)
